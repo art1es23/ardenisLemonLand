b16a0853285187204f39bd7703792d6c36e2b51a
Denys Symak <dreamwinamounts@gmail.com>
2020-11-16 18:11:25 +0200

    v1.0.4

--- src/js/main.js
+++ src/js/main.js
@@ -1,64 +1,5 @@
 let s = document.querySelectorAll('.section');
 
-/*
-for (let i = 0; i <= s.length; i++) {
-    s[i].style.transform = `rotateZ(${60 * i}deg)`;
-}
-*/
-
-/*
-window.addEventListener('wheel', e => {
-
-    e.preventDefault();
-    console.log(e);
-    e = e || window.event;
-
-    let delta = e.deltaY || e.detail;
-
-    console.log(delta);
-
-    if (delta === 3) {
-
-        for (let i = 1; i <= s.length; i++) {
-            let rotateAngle = s[i].style.transform;
-            s[i].style.transform = `rotateZ(${rotateAngle} - 60)`;
-            console.log('3: ', s[i].style.transform)
-        }
-    } else if (delta === -3) {
-
-        for (let i = 1; i <= s.length; i++) {
-            s[i].style.transform = '+60deg';
-            console.log('-3:', s[i].style.transform)
-        }
-    }
-
-})
-*/
-
-/*
-let info = window.getComputedStyle(s, null);
-console.log('sadas: ', info);
-let tr = info.getPropertyValue("transform") ||
-    info.getPropertyValue("-o-transform") ||
-    info.getPropertyValue("-webkit-transform") ||
-    info.getPropertyValue("-moz-transform") ||
-    info.getPropertyValue("-ms-transform");
-
-console.log('MATRIX', tr);
-
-let x = tr.match(/matrix\((?:\d+, ?){4}(\d+)/);
-console.log('some: ', x);
-let values = tr.split('(')[1].split(')')[0].split(',');
-let a = values[0];
-let b = values[1];
-
-// next line works for 30deg but not 130deg (returns 50);
-// let angle = Math.round(Math.asin(sin) * (180/Math.PI));
-let angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
-
-console.log('Rotate: ' + angle + 'deg');
-*/
-
 for (let i = 0; i <= s.length; i++) {
     let info = window.getComputedStyle(s[i], null);
     console.log(`${i}: `, info);
@@ -112,46 +53,6 @@ for (let i = 0; i <= s.length; i++) {
     })
 
 }
-
-/*            let results = info.match(/matrix(?:(3d)\(\d+(?:, \d+)*(?:, (\d+))(?:, (\d+))(?:, (\d+)), \d+\)|\(\d+(?:, \d+)*(?:, (\d+))(?:, (\d+))\))/)
-                        console.log('res: ', results)*/
-
-
-/*
-
-      transform: rotateZ($angle * ($i - 1))
-window.addEventListener('wheel', (e) => {
-    e.preventDefault();
-    console.log(e);
-    e = e || window.event;
-
-    let target = e.target;
-    let delta = e.deltaY || e.detail;
-
-    console.log(delta);
-
-    if (delta === 3) {
-
-        for (let i = 1; i <= s.length; i++) {
-            let transformRotate = s[i].style.transform;
-            console.log(transformRotate);
-            s[i].style.transform += transformRotate;
-            console.log(`change ${i}`, s[i].style.transform)
-        }
-    } else if (delta === -3) {
-        for (let i = 1; i <= s.length; i++) {
-            let transformRotate = s[i].style.transform;
-            console.log(transformRotate);
-            s[i].style.transform = transformRotate + `rotateZ(${60 * i - 60}deg)`;
-            transformRotate = s[i].style.transform;
-            console.log(`change ${i}`, transformRotate)
-        }
-    }
-})
-*/
-
-
-
 
 
 /*
